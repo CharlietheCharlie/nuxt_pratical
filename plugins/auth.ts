79133123cba973c9323019f207db0452d0cd533d
Charlie Wang <st896218@gmail.com>
2023-08-21 15:17:45 +0800

    console

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -5,7 +5,6 @@ export default defineNuxtPlugin(() => {
   
     addRouteMiddleware('auth',() => {
       const { $auth } = useNuxtApp();
-      console.log($auth);
       if(!$auth?.currentUser?.uid){
         return abortNavigation();
       }
